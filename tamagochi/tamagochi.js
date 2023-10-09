925b9ed0d145a8ebb1927c3865c5b3d96a1883f2
David Camps <davidcamps2000@gmail.com>
2023-10-09 19:47:48 +0200

    d

--- tamagochi/tamagochi.js
+++ tamagochi/tamagochi.js
@@ -1,139 +1,134 @@
-let progressCircle = document.querySelector(".progress");
+let comprova = true;
 
 const divGeneralMrBean = document.getElementById("divGeneralMrBean");
 
 let progressComida = document.querySelector("#progressComida");
 let progressJugar = document.querySelector("#progressJugar");
+let progressLuchar = document.querySelector("#progressLuchar");
 
-let radius = progressCircle.r.baseVal.value;
+
 let radius1 = progressComida.r.baseVal.value;
 let radius2 = progressJugar.r.baseVal.value;
+let radius3 = progressJugar.r.baseVal.value;
 
 
-let circumference = radius * 2 * Math.PI;
 let circumference1 = radius1 * 2 * Math.PI;
 let circumference2 = radius2 * 2 * Math.PI;
+let circumference3 = radius3 * 2 * Math.PI;
 
-progressCircle.style.strokeDasharray = circumference;
+
 progressComida.style.strokeDasharray = circumference1;
 progressJugar.style.strokeDasharray = circumference2;
+progressLuchar.style.strokeDasharray = circumference3;
 
-let progress = 100;
+let progress;
 let progress1 = 100;
 let progress2 = 100;
-const numMaximo = 100;
+let progress3 = 100;
 
-document.getElementById("divComida").addEventListener("click", ()=>{
-    
-  
-   
-        progress1 = progress1 + 3;
-    
-    
-    
+
+
+
+
+document.getElementById("divComida").addEventListener("click", () => {
+
+  progress1 = progress1 + 4;
+  document.getElementById("perComida").innerText = progress1 + "%";
+
+  if (progress1 != 0 || progress2 != 0 || progress3!=0) {
+    progress = (progress1 + progress2 + progress3) / 3;
+    document.getElementById("h3de").innerText = Number(progress.toFixed(2)) + "%";
+  }
+});
+
+document.getElementById("divJugar").addEventListener("click", () => {
+
+  progress2 = progress2 + 4;
+  document.getElementById("percJugar").innerText = progress2 + "%";
+  if (progress1 != 0 || progress2 != 0 || progress3!=0) {
+    progress = (progress1 + progress2 + progress3) / 3;
+    document.getElementById("h3de").innerText = Number(progress.toFixed(2)) + "%";
+
+  }
 })
 
-document.getElementById("divJugar").addEventListener("click", ()=>{
-    
-  
-   
-    progress2 = progress2 + 3;
+document.getElementById("divLuchar").addEventListener("click", () => {
 
+  progress3 = progress3 + 4;
+  document.getElementById("perLuchar").innerText = progress3 + "%";
+  if (progress1 != 0 || progress2 != 0 || progress3!=0) {
+    progress = (progress1 + progress2 + progress3) / 3;
+    document.getElementById("h3de").innerText = Number(progress.toFixed(2)) + "%";
 
-
+  }
 })
-setProgress();
 
-function setProgress() {
 
-    progressCircle.style.strokeDashoffset = circumference - (progress / 100) * circumference;
-    progress--;
 
-}
 function setProgressComida() {
 
-    progressComida.style.strokeDashoffset = circumference1 - (progress1 / 100) * circumference1;
-    progress1--;
+  progressComida.style.strokeDashoffset = circumference1 - (progress1 / 100) * circumference1;
+  progress1--;
 
 }
 function setProgressJugar() {
 
-    progressJugar.style.strokeDashoffset = circumference2 - (progress2 / 100) * circumference2;
-    progress2--;
+  progressJugar.style.strokeDashoffset = circumference2 - (progress2 / 100) * circumference2;
+  progress2--;
 
 }
 
-function startMrBean(){
-    for (let i = 0; i <= 100; i++) {
-
-        (function (index) {
-            setTimeout(function () {
-                if(progress <= 150 && progress >= 65){
-                    divGeneralMrBean.style.backgroundColor="rgb(39, 242, 12)";
-                }else if(progress < 65 && progress > 30){
-                    divGeneralMrBean.style.backgroundColor="orange";
-                }else {
-                    //document.getElementById("imagen").src = "./mrbeansaludo.png";
-                    //document.getElementById("imagen").style.width = "60px";
-                    divGeneralMrBean.style.backgroundColor="red";
-                    
-                }
-              setProgress();
-              if(progress >= 0){
-                document.getElementById("h3de").innerText = progress + "%";
-              }else{
-                document.getElementById("h3de").innerText = "0%";
-              }
-              
-            }, 600 * index);
-          })(i);
-               
-    }
+function setProgressLuchar(){
+  progressLuchar.style.strokeDashoffset = circumference3 - (progress3 / 100) * circumference3;
+  progress3--;
 }
 
-if(progress1 == 0){
-    startMrBean();
-}
-for (let i = 0; i <= 100; i++) {
 
-    (function (index) {
-        setTimeout(function () {          
-          setProgressComida();        
-          if(progress >= 0){
-            document.getElementById("h3de1").innerText = progress1 + "%";
-          }else{
-            document.getElementById("h3de1").innerText = "0%";
-          }
-        }, 500 * index);
-      })(i);
-           
-}
 
-for (let i = 0; i <= 100; i++) {
 
-    (function (index) {
-        setTimeout(function () {          
-          setProgressJugar();        
-          if(progress >= 0){
-            document.getElementById("h3de2").innerText = progress2 + "%";
-          }else{
-            document.getElementById("h3de2").innerText = "0%";
-          }
-        }, 800 * index);
-      })(i);
-           
-}
 
-/*document.getElementById("pol").addEventListener("click", ()=>{*/
-    document.getElementById("pol").disabled = true;
-    
-    
-/*});*/
 
-        
-        
-        
-        
+// Establece un intervalo para ejecutar la función cada 1 segundo (1000 milisegundos)
+
+
+
+
+
+
+const bucleComida = setInterval(() => {
+  if (progress1 > 0) {
+    setProgressComida();
+    document.getElementById("perComida").innerText = progress1 + "%";
+  } else {
+    clearInterval(bucleComida);
+  }
+}, 800);
+
+const bucleJugar = setInterval(() => {
+  if (progress2 > 0) {
+    setProgressJugar();
+    document.getElementById("percJugar").innerText = progress2 + "%";
+  } else {
+    clearInterval(bucleComida);
+  }
+}, 1000);
+
+
+const bucleLuchar = setInterval(() => {
+  if(progress3>0){
+    setProgressLuchar();
+    document.getElementById("perLuchar").innerText = progress3 + "%";
+  }else{
+    clearInterval(bucleLuchar);
+  }
+}, 500);
+
+
+
+
+
+
+
 
 
 
